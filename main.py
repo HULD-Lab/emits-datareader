a219cba6c424c3e47108103ba02ae0f48280d297
Jiří Pešík <jiri.pesik@huld.io>
2020-03-10 17:23:06 +0100

    Solves #6

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ end = account.default_timezone.localize(EWSDateTime.now())
 emits_email_list = account.inbox.filter(
     sender=settings.jsonConfig["emits_sender"],
     subject__icontains="Invitation To Tender",
-    datetime_received__range=(start, end)
+    datetime_received__range=(start, end),
 )
 
 collection = settings.get_mongo_collection()
